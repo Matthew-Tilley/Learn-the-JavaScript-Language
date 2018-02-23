ce434295dab385f9d73d70696131ced17f1a8f8d
Matt Tilley <mtilley255@gmail.com>
2018-02-23 10:12:13 -0600

    2.2 Tinkering with Math.random() and other stuff.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+
+
+
+/* Section 2.2
+
+console.log(Math.random());
+
+console.log(Math.ceil(Math.random()));
+
+console.log(Math.ceil(Math.random()-Math.random()));
+ */
+
+/* May have just stumbled upon something I've been doing wrong?
+I put in the code below and it gave me back the method for 
+Math.random:
+console.log(Math.random); */
+
+/* Conversely, when it occurred to me that I omitted the
+parens (because Math.random() is a method) it worked 
+properly.  Wonder if maybe something this simple and silly
+is what has been giving me headaches?   */
+
+/* I did not know these were a part of JavaScript:
+Infinity;
+-Infinity; */
+
+
+
+/*   Section 2.1
+
 var x = 32;
 
 console.log(x);
@@ -6,4 +36,4 @@ x = 45;
 
 console.log(x);
 
-console.log(x + " boo");
+console.log(x + " boo"); */
